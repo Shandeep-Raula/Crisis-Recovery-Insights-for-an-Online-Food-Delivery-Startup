cf366c6d036748ee2e2a4d2cd142636021564961
Shandeep-Raula <shandeepra@gmail.com>
2025-10-25 08:32:53 +0530

    csv files time stamp is changed and added file foemat for csv sql scripts

--- SQL/Create_table.sql
+++ SQL/Create_table.sql
@@ -4,39 +4,41 @@ CREATE OR REPLACE SCHEMA QUICK_BITE.RAW;
 USE SCHEMA QUICK_BITE.RAW;
 
 -- dim_customer
-CREATE OR REPLACE TABLE dim_customer (
-    customer_id INT PRIMARY KEY,
-    signup_date DATE,
-    city STRING,
-    acquisition_channel STRING
+CREATE OR REPLACE TABLE RAW.DIM_CUSTOMER (
+    CUSTOMER_ID VARCHAR PRIMARY KEY,
+    SIGNUP_DATE DATE,
+    CITY STRING,
+    ACQUISITION_CHANNEL STRING
 );
 
 -- dim_delivery_partner
 CREATE OR REPLACE TABLE dim_delivery_partner (
-    delivery_partner_id INT PRIMARY KEY,
+    delivery_partner_id VARCHAR PRIMARY KEY,
     partner_name STRING,
     city STRING,
     vehicle_type STRING,
     employment_type STRING,
-    avg_rating FLOAT,
+    avg_rating NUMBER(10,2),
     is_active BOOLEAN
 );
 
+
 -- dim_restaurant
 CREATE OR REPLACE TABLE dim_restaurant (
-    restaurant_id INT PRIMARY KEY,
+    restaurant_id VARCHAR PRIMARY KEY,
     restaurant_name STRING,
     city STRING,
     cuisine_type STRING,
     partner_type STRING,
-    avg_prep_time_min INT,
-    is_active BOOLEAN
+    avg_prep_time_min STRING,
+    is_active STRING
 );
+
 
 -- dim_menu_item
 CREATE OR REPLACE TABLE dim_menu_item (
-    menu_item_id INT PRIMARY KEY,
-    restaurant_id INT,
+    menu_item_id VARCHAR PRIMARY KEY,
+    restaurant_id VARCHAR,
     item_name STRING,
     category STRING,
     is_veg BOOLEAN,
@@ -46,10 +48,10 @@ CREATE OR REPLACE TABLE dim_menu_item (
 
 -- fact_orders
 CREATE OR REPLACE TABLE fact_orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT,
-    restaurant_id INT,
-    delivery_partner_id INT,
+    order_id VARCHAR PRIMARY KEY,
+    customer_id VARCHAR,
+    restaurant_id VARCHAR,
+    delivery_partner_id VARCHAR,
     order_timestamp TIMESTAMP_NTZ,
     subtotal_amount NUMBER(10,2),
     discount_amount NUMBER(10,2),
@@ -64,19 +66,19 @@ CREATE OR REPLACE TABLE fact_orders (
 
 -- fact_delivery_performance
 CREATE OR REPLACE TABLE fact_delivery_performance (
-    order_id INT PRIMARY KEY,
+    order_id VARCHAR PRIMARY KEY,
     actual_delivery_time_mins INT,
     expected_delivery_time_mins INT,
-    distance_km FLOAT,
+    distance_km NUMBER(10,1),
     FOREIGN KEY (order_id) REFERENCES fact_orders(order_id)
 );
 
 -- fact_order_items
 CREATE OR REPLACE TABLE fact_order_items (
-    order_id INT,
-    item_id INT PRIMARY KEY,
-    menu_item_id INT,
-    restaurant_id INT,
+    order_id VARCHAR,
+    item_id VARCHAR PRIMARY KEY,
+    menu_item_id VARCHAR,
+    restaurant_id VARCHAR,
     quantity INT,
     unit_price NUMBER(10,2),
     item_discount NUMBER(10,2),
@@ -88,13 +90,13 @@ CREATE OR REPLACE TABLE fact_order_items (
 
 -- fact_ratings
 CREATE OR REPLACE TABLE fact_ratings (
-    order_id INT PRIMARY KEY,
-    customer_id INT,
-    restaurant_id INT,
+    order_id VARCHAR PRIMARY KEY,
+    customer_id VARCHAR,
+    restaurant_id VARCHAR,
     rating INT,
     review_text STRING,
     review_timestamp TIMESTAMP_NTZ,
-    sentiment_score FLOAT,
+    sentiment_score NUMBER(10,2),
     FOREIGN KEY (order_id) REFERENCES fact_orders(order_id),
     FOREIGN KEY (customer_id) REFERENCES dim_customer(customer_id),
     FOREIGN KEY (restaurant_id) REFERENCES dim_restaurant(restaurant_id)
